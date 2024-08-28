41ee250e07e11b1643ee739212f247b538262e73
louck <luc.parent@commown.coop>
2024-08-28 14:04:47 +0200

    Add mail_channel field on partner and related company field on mail_channel

---
+++ commown_b2b_mail_channel/models/mail_channel.py
@@ -0,0 +1,36 @@
+from odoo import api, fields, models
+
+
+class MailChannel(models.Model):
+    _inherit = "mail.channel"
+
+    company_ids = fields.One2many(
+        "res.partner",
+        inverse_name="mail_channel_id",
+        domain=[("is_company", "=", "True")],
+    )
+
+    company_id = fields.Many2one(
+        "res.partner",
+        string="Support channel of",
+        compute="_compute_company_id",
+        inverse="_inverse_company_id",
+        domain=[("is_company", "=", "True")],
+        store=False,
+    )
+
+    @api.depends("company_ids")
+    def _compute_company_id(self):
+        for rec in self.filtered("company_ids"):
+            rec.company_id = rec.company_ids[0]
+
+    def _inverse_company_id(self):
+        for rec in self:
+            chan = rec
+            new_company = rec.company_id
+
+            self.env["res.partner"].search([("mail_channel_id", "=", rec.id)]).update(
+                {"mail_channel_id": False}
+            )
+
+            chan.company_ids = new_company
