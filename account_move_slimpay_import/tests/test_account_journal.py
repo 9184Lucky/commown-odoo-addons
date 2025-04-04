5b96d61fbf66651bea9c39ca83726a3423be4e4d
louck <luc.parent@commown.coop>
2025-04-04 18:07:20 +0200

    fix tests

--- account_move_slimpay_import/tests/test_account_journal.py
+++ account_move_slimpay_import/tests/test_account_journal.py
@@ -15,6 +15,14 @@ class AccountJournalTC(TransactionCase):
         with open(HERE / fname, "rb") as fobj:
             csv_content = fobj.read()
 
+        account_receivable = self.env["account.account"].create(
+            {
+                "code": "TEST.RECE",
+                "name": "Test receivable",
+                "account_type": "asset_cash",
+            }
+        )
+        journal.receivable_account_id = account_receivable
         self.importer = self.env["credit.statement.import"].create(
             {
                 "journal_id": journal.id,
@@ -31,7 +39,7 @@ class AccountJournalTC(TransactionCase):
 
         imported_move = self.env[action["res_model"]].browse(action["res_id"])
         self.assertEqual(imported_move.state, "draft")
-        self.assertEqual(imported_move.amount, 85)
+        self.assertEqual(imported_move.amount_total, 85)
         self.assertEqual(len(imported_move.line_ids), 7)
         self.assertTrue(
             float_is_zero(
